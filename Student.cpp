abbe8b77237aa94bba05d332fb85c094369e8627
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-01-05 09:23:54 -0800

    Update Student.cpp
    Changing after updating Student.h file

--- Student.cpp
+++ Student.cpp
@@ -1,43 +1,40 @@
 #include "Student.h"
-#include <iostream> 
+#include <iostream>
 
-Student::Student() : studentId(""), gpa(0.0) {}
 
-Student::Student(const std::string& name, int age, const std::string& studentId) : Person(name, age), studentId(studentId), gpa(0.0) {}
+Student::Student() : Person(), studentId(""), gpa(0.0) {}
+
+
+Student::Student(const std::string& name, const std::string& studentId)
+    : Person(name), studentId(studentId), gpa(0.0) {}
+
 
 void Student::inputDetails() {
-    std::cout  << "Enter student details: " << std::endl;
+    std::cout << "Enter student details:" << std::endl;
+
 
     setName("");
-    setAge(0);
-
+    
     std::cout << "Student ID: ";
-    std::getLine(std::cin, studentId);
+    std::getline(std::cin, studentId);
+
     std::cout << "GPA: ";
-    std::cin.ignore();
+    std::cin >> gpa;
+    std::cin.ignore(); 
 }
+
 
 void Student::displayStudent() const {
-
+    
     display();
     std::cout << "Student ID: " << studentId << ", GPA: " << gpa << std::endl;
-
 }
+
 
 const std::string& Student::getStudentId() const {
-    
     return studentId;
-
 }
 
-const std::string& Student::getStudentId() const {
-
-    return studentId;
-
-}
-
-double Student::getGPA() const { 
-
+double Student::getGPA() const {
     return gpa;
-
 }
