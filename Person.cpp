09f5f3d588b15a98dc5645faa9287e84917a7b24
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-01-05 09:22:02 -0800

    Update Person.cpp
    No new changes, but updating from the Person.cpp file.

--- Person.cpp
+++ Person.cpp
@@ -1,23 +1,17 @@
 #include "Person.h"
-#include <iostream> 
+#include <iostream>
 
-Person::Person() : name(""), age (0) {} 
-Person::Person(const std::string& name, int age) : name(name), age(age) {}
 
-voidPerson::setName(const std::string& newName) {
+Person::Person() : name("") {}
 
+
+Person::Person(const std::string& name) : name(name) {}
+
+
+void Person::setName(const std::string& newName) {
     name = newName;
-
-}
-
-void Person::setAge(int newAge) {
-    
-    age = newAge;
-
 }
 
 void Person::display() const {
-
-    std::cout << "Name: " << name << ", Age: " << age << std::endl;
-
+    std::cout << "Name: " << name << std::endl;
 }
