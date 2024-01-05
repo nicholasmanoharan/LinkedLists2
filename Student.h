a962d1b0e55733912a0c0b6fff69757b2b97d6b1
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-01-05 09:23:07 -0800

    Update Student.h
    Code was not allowing the user to input Student name

--- Student.h
+++ Student.h
@@ -1,17 +1,24 @@
-#ifndef STUDENT_H
-#define STUDENT_H
+#ifndef STUDENT_H_
+#define STUDENT_H_
+
 #include "Person.h"
+#include <string>
 
 class Student : public Person {
-    public: 
-        Student();
-        Student(const std::strig& name, int age, const std::string& studentId);
-        void inputDetails();
-        void displayStudent() const;
-        const std::string& getStudentId() const;
-        double getGPA() const;
+public:
+   
+    Student();
+    explicit Student(const std::string& name, const std::string& studentId);
 
-    private: 
-        sdt::string studentId(); 
-        double gpa;
+   
+    void inputDetails();
+    void displayStudent() const;
+    const std::string& getStudentId() const;
+    double getGPA() const;
+
+private:
+    std::string studentId;
+    double gpa;
 };
+
+#endif 
